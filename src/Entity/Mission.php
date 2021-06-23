dacb638369120ab5652a6a0737d7adc0fade5d26
Timothée <zingouingouin@hotmail.com>
2021-06-23 16:48:14 +0200

    ajout de l'enity mission

---
+++ src/Entity/Mission.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MissionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MissionRepository::class)
+ */
+class Mission
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $descriptif;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date_debut;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date_fin;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date_facture;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $montant;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDescriptif(): ?string
+    {
+        return $this->descriptif;
+    }
+
+    public function setDescriptif(string $descriptif): self
+    {
+        $this->descriptif = $descriptif;
+
+        return $this;
+    }
+
+    public function getDateDebut(): ?\DateTimeInterface
+    {
+        return $this->date_debut;
+    }
+
+    public function setDateDebut(\DateTimeInterface $date_debut): self
+    {
+        $this->date_debut = $date_debut;
+
+        return $this;
+    }
+
+    public function getDateFin(): ?\DateTimeInterface
+    {
+        return $this->date_fin;
+    }
+
+    public function setDateFin(\DateTimeInterface $date_fin): self
+    {
+        $this->date_fin = $date_fin;
+
+        return $this;
+    }
+
+    public function getDateFacture(): ?\DateTimeInterface
+    {
+        return $this->date_facture;
+    }
+
+    public function setDateFacture(\DateTimeInterface $date_facture): self
+    {
+        $this->date_facture = $date_facture;
+
+        return $this;
+    }
+
+    public function getMontant(): ?float
+    {
+        return $this->montant;
+    }
+
+    public function setMontant(float $montant): self
+    {
+        $this->montant = $montant;
+
+        return $this;
+    }
+}
