d81f304765995a50357c15e74a7791285247469d
Timothée <zingouingouin@hotmail.com>
2021-06-28 15:09:58 +0200

    accès base de données OKAY

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProfileController extends AbstractController
 {
     /**
-     * @Route("/profile", name="profil")
+     * @Route("/profile", name="profile")
      */
     public function index(): Response
     {
