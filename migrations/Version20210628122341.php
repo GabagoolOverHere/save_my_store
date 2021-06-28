882f350df12a859769a574bd8efd09b2de2ae0d1
LilFumanal <charlie.fumanal@gmail.com>
2021-06-28 15:59:49 +0200

    Suppression effective des tables inutiles

--- migrations/Version20210628122341.php
+++ migrations/Version20210628122341.php
@@ -21,13 +21,11 @@ final class Version20210628122341 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE probleme CHANGE intitule intitule LONGTEXT DEFAULT NULL');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_857D55ADF85E0677 ON restaurant_owner (username)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE probleme CHANGE intitule intitule LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
-        $this->addSql('DROP INDEX UNIQ_857D55ADF85E0677 ON restaurant_owner');
     }
 }
